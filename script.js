ff07e132d584ee8043fae8fad707429f9b51b95b
xadvent <piercestrutt@gmail.com>
2023-07-27 15:08:13 -0600

    Added checkWin fx on each turn.
    checkWin prints winner in console if wrong

--- script.js
+++ script.js
@@ -12,69 +12,74 @@ const squares = (function () {
     const allButtons = document.querySelectorAll('.tile')
     return { allButtons }
 })()
+
 // Makes the Grid
+const placeMarkers = (function () {
 
-const counter = function () {
-    let _count = 1;
+    const counter = function () {
+        let _count = 1;
+        const check = () => {
+            return _count % 2 === 0
+        }
+        const addOne = () => _count++
 
-    const check = () => {
-        return _count % 2 === 0
+        return {
+            addOne,
+            check
+        }
     }
 
-    const addOne = () => _count++
+    //  should check for a win 
+    const checkWin = function(marker) {
+        const getMarkerList = (function(){
+            const markerList = document.querySelectorAll(`.${marker}`)
+            const selectedTiles = []
+            markerList.forEach(mark => {
+                const classMark = mark.classList.value.split(' ')[1]
+                selectedTiles.push(+classMark)
+            });
+            return {selectedTiles}
+        })(); const selectedTiles = getMarkerList.selectedTiles.sort()
 
-    const symbol = () => {
+         const winning = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
-    }
+         console.log(selectedTiles)
+        
+         winning.forEach(set => {
+            return set.every(r => selectedTiles.includes(r)) ? console.log(marker + ' Wins') : null
+         })
+    }; 
+
+    // turnChooser should select what element is going to be placed
+    const addEvents = function () {
+        const test = counter()
+
+        const alternate = function () {
+            let thing = test.check()
+            if (Array.from(this.classList).includes('X') || Array.from(this.classList).includes('O')) return
+            thing ? this.classList.add('O') : this.classList.add('X')
+            test.addOne()
+
+            placeMarkers.checkWin('X')
+            return
+        }
+
+        const addEventListen = (function () {
+            const { allButtons } = squares
+            allButtons.forEach(button => button.addEventListener('click', alternate))
+        })()
+
+    }; addEvents()
 
     return {
-        addOne,
-        check
+        checkWin
     }
-}
-
-// turnChooser should select what element is going to be placed
-const addEvents = function () {
-    const test = counter()
-
-    const alternate = function () {
-        let thing = test.check()
-        if (Array.from(this.classList).includes('X') || Array.from(this.classList).includes('O')) return
-        thing ? this.classList.add('O') : this.classList.add('X')
-        test.addOne()
-        return
-    }
-
-    const addEventListen = (function () {
-        const { allButtons } = squares
-        allButtons.forEach(button => button.addEventListener('click', alternate))
-    })()
-
-    return {
-        alternate
-    }
-}; addEvents()
-
-const Player = function (marker) {
-    const { check } = counter()
-    const placeMarker = function () {
-
-    }
-
-    return {
-        placeMarker
-    }
-}
-
-
+})()
 
 
 /*
-2. Decide who goes first
-    Put person functions inside 
-3. add event listeners depending on the counter
-4. Add markder to corresponding div
-5. Change event listener
 6. Write down potential winning lines
 7. Check for win on each turn
+8. Add a restart button
+9. Create an option to select player or Bot
 */
